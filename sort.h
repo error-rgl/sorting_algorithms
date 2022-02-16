1c7364628d46e42c2bfaef65d83c6814959e5d0c
error-rgl <rglcnd@gmail.com>
2022-02-16 06:00:11 +0100

    Add header file

---
+++ sort.h
@@ -0,0 +1,31 @@
+#ifndef _SORT_H
+#define _SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void node_change(listint_t **list1, listint_t **listt, listint_t **list);
+void selection_sort(int *array, size_t size);
+
+void quick_sort(int *array, size_t size);
+void quick_sorting(int *array, size_t size, int *full_array, size_t full_size);
+
+#endif 
